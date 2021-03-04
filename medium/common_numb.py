3d3f6c8e620c45ff5ca9f104b07f6d7b41c7b26b
Gronomix <Gronomix2013@gmail.com>
2021-03-04 18:41:28 +0300

    выполнил

--- medium/common_numb.py
+++ medium/common_numb.py
@@ -4,3 +4,25 @@
 
 Функция должна вернуть список общих чисел, который отсортирован по убыванию
 """
+
+some_list1 = [1, 2, 3]
+some_list2 = [7, 11, 33]
+
+def common_numbers(some_list1: list, some_list2: list):
+    common_list = []
+    for i in some_list1 and i in some_list2:
+        if i == int:
+            common_list.append(i)
+            common_list.sort(reverse=True)
+    else:
+        print('ERROR')
+    return common_list
+
+
+
+print(common_list)
+
+if __name__ == '__main__':
+    print(common_numbers)
+
+
