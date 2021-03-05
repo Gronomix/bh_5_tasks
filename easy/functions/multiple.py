58246460d0ed63614f4aafe2674b4e4c99da4961
Gronomix <Gronomix2013@gmail.com>
2021-03-05 17:49:47 +0300

    решено

--- easy/functions/multiple.py
+++ easy/functions/multiple.py
@@ -2,3 +2,18 @@
 Написать функцию multiply, которая принимает аргумент n.
 и возвращает функцию closure, которая принимает аргумент x и возвращает x * n
 """
+
+
+def multiply(n: int):
+    def closure(x: int):
+        result = n * x
+        return result
+    return closure
+
+
+
+if __name__ == '__main__':
+    add_two = multiply(2)
+    print(add_two(123))
+
+
