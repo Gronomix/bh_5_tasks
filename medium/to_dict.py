884bae04577fe402f81477d69100e69a300e3a32
Gronomix <Gronomix2013@gmail.com>
2021-03-13 12:48:38 +0300

    решено

--- medium/to_dict.py
+++ medium/to_dict.py
@@ -3,4 +3,14 @@
 возвращает словарь, в котором каждый элемент списка является и ключом и
 значением. Предполагается, что элементы списка будут соответствовать
 правилам задания ключей в словарях.
+
 """
+
+some_list = [1, 2, 3, 4]
+def to_dict(some_list: list):
+    new_dict = dict(zip(some_list, some_list))
+    return new_dict
+
+
+slist = to_dict(some_list)
+print(slist)
