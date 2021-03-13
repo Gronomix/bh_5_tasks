b15c362e63612f64a05b00454f856de8a0274e85
Gronomix <Gronomix2013@gmail.com>
2021-03-13 12:43:04 +0300

    решено

--- medium/reverse_dict.py
+++ medium/reverse_dict.py
@@ -4,4 +4,13 @@
 ключи первого
 
 Тело функции может состоять из одной строки!
+
 """
+some_dict = {'one': 1, 'two': 2, 'tree': 3}
+
+def reverse_dict(some_dict: dict):
+    new_dict = dict(zip(some_dict.values(), some_dict.keys()))
+    return new_dict
+
+sdict = reverse_dict(some_dict)
+print(sdict)
