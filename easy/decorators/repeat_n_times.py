2dac065845b30fa4a76ce34a9fa51553331d295e
Gronomix <Gronomix2013@gmail.com>
2021-03-15 17:58:12 +0300

    Не получается решить

--- easy/decorators/repeat_n_times.py
+++ easy/decorators/repeat_n_times.py
@@ -4,3 +4,21 @@
 Написать декоратор repeat_n_times, у которого есть параметр n.
 Декоратор должен выполнить функцию bang n раз
 """
+
+
+def repeat_n_times(bang):
+    def wrapper(n):
+        print(f"{bang * n}")
+
+    return wrapper
+
+
+def bang(n):
+    print('BOOM \n')
+    return
+
+bang_func = bang(6)
+
+
+
+print(bang_func)
