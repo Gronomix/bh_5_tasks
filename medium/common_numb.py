bc3f59c924a3ad1d12a94f3f787a653a32c2aa9d
Gronomix <Gronomix2013@gmail.com>
2021-03-15 17:26:19 +0300

    решено

--- medium/common_numb.py
+++ medium/common_numb.py
@@ -5,24 +5,25 @@
 Функция должна вернуть список общих чисел, который отсортирован по убыванию
 """
 
-some_list1 = [1, 2, 3]
-some_list2 = [7, 11, 33]
+some_list1 = [1, 2, 3, 11, 25, 5, 8]
+some_list2 = [7, 11, 33, 8, 25]
 
 def common_numbers(some_list1: list, some_list2: list):
     common_list = []
-    for i in some_list1 and i in some_list2:
-        if i == int:
+
+    for i in some_list1:
+        if i in some_list2:
+
             common_list.append(i)
             common_list.sort(reverse=True)
-    else:
-        print('ERROR')
+
+
     return common_list
 
 
 
-print(common_list)
 
-if __name__ == '__main__':
-    print(common_numbers)
 
+sum_list = common_numbers(some_list1,some_list2)
+print(sum_list)
 
