9dabfdb1b4a4c88ee0e0f78ba61bfbca3c487909
Gronomix <Gronomix2013@gmail.com>
2021-03-15 16:42:04 +0300

    решено

--- medium/check_number.py
+++ medium/check_number.py
@@ -5,3 +5,22 @@ False, если нет
 
 Нельзя пользоваться операцией возведения в степень
 """
+
+def check_number(n):
+
+    i = 1
+    result = 0
+    while i < n:
+        i = i * 2
+    if i == n:
+        result = 1
+    else:
+        result = 0
+    if result == 1:
+        return True
+    else:
+        return False
+
+num = check_number(3)
+print(num)
+
