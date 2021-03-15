5b688deffdee0534b3d56ed0c938e196a7f002c7
Gronomix <Gronomix2013@gmail.com>
2021-03-15 18:25:10 +0300

    Непонимаю где ошибка

--- easy/generators/triangular_numbers.py
+++ easy/generators/triangular_numbers.py
@@ -17,3 +17,12 @@ next(tn_gen) -> 2
 next(tn_gen) -> 6
 next(tn_gen) -> 24
 """
+
+def triangular_numbers(n):
+    for i in range(1, n + 1):
+        tn = 1 / 2 * n * (n + 1)
+        return tn
+
+tn_gen = triangular_numbers(6)
+
+print(next(tn_gen))
