3ff4575d0dae95d8ea6d0b91b23eb2a6672b5e8d
Gronomix <Gronomix2013@gmail.com>
2021-03-15 16:49:46 +0300

    решено

--- medium/mult_dict_val.py
+++ medium/mult_dict_val.py
@@ -4,4 +4,13 @@
 
 Необходимо, чтобы функция вернула результат умножения всех значений из словаря
 """
-SOME_DICT = {str(val): val for val in range(1, 50, 3)}
+SOME_DICT = {str(val): val for val in range(1, 20, 3)}
+
+def multiply_dict_values(SOME_DICT: dict):
+
+    result = sum(SOME_DICT.values())
+    return result
+
+
+sum_val = multiply_dict_values(SOME_DICT)
+print(sum_val)
